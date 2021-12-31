d5a626953b6a707354be2a8ee302afbdb8cdf985
ponzu840w <ponzu840w@Redmax.localdomain>
2021-12-31 20:51:25 +0900

    VideoTimingGenのジェネリック記述書き途中

--- VideoTimingGen.vhd
+++ VideoTimingGen.vhd
@@ -6,32 +6,33 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
+-- constants
+-- clock length
+-- H
+generic(H_VALID : integer range 0 to 256 := 256;
+       );
+constant H_FRONT : integer := 6;
+constant H_SYNC : integer := 34;
+constant H_BACK : integer := 35;
+-- V
+constant V_VALID : integer := 768;
+constant V_FRONT : integer := 3;
+constant V_SYNC : integer := 6;
+constant V_BACK : integer := 29;
+-- bas width
+constant H_CNT_WIDTH  : integer := 9;
+constant V_CNT_WIDTH  : integer := 10;
+constant H_OUT_WIDTH  : integer := 8;
+constant V_OUT_WIDTH  : integer := 10;
 
 -- entity declaration
 entity VideoTimingGen is
-  -- constants
-  -- clock length
-  -- H
-  constant H_VALID : integer := 256;
-  constant H_FRONT : integer := 6;
-  constant H_SYNC : integer := 34;
-  constant H_BACK : integer := 35;
-  -- V
-  constant V_VALID : integer := 768;
-  constant V_FRONT : integer := 3;
-  constant V_SYNC : integer := 6;
-  constant V_BACK : integer := 29;
-  -- bas width
-  constant H_CNT_WIDTH  : integer := 9;
-  constant V_CNT_WIDTH  : integer := 10;
-  constant H_OUT_WIDTH  : integer := 8;
-  constant V_OUT_WIDTH  : integer := 10;
-  port (
+  port(
     clk_in, reset_in  : in std_logic;
     h_blank_out, v_blank_out, h_sync_out, v_sync_out  : out std_logic;
     h_addr_out  : out std_logic_vector(H_OUT_WIDTH-1 downto 0);
     v_addr_out  : out std_logic_vector(V_OUT_WIDTH-1 downto 0)
-  );
+  )
 end VideoTimingGen;
 
 architecture RTL of VideoTimingGen is
