0e9304bbbca71363dfbd6b5019e482691f9e34b2
mulwak <react@Surmonomicon>
2021-12-31 21:23:26 +0900

    VideoTimingGenのコンパイルがとりあえずとおる

--- VideoTimingGen.vhd
+++ VideoTimingGen.vhd
@@ -6,33 +6,34 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
--- constants
--- clock length
--- H
-generic(H_VALID : integer range 0 to 256 := 256;
-       );
-constant H_FRONT : integer := 6;
-constant H_SYNC : integer := 34;
-constant H_BACK : integer := 35;
--- V
-constant V_VALID : integer := 768;
-constant V_FRONT : integer := 3;
-constant V_SYNC : integer := 6;
-constant V_BACK : integer := 29;
--- bas width
-constant H_CNT_WIDTH  : integer := 9;
-constant V_CNT_WIDTH  : integer := 10;
-constant H_OUT_WIDTH  : integer := 8;
-constant V_OUT_WIDTH  : integer := 10;
-
 -- entity declaration
 entity VideoTimingGen is
+  -- constants
+  generic(
+  -- clock length
+  -- H
+     H_VALID : integer range 0 to 256 := 256;
+     H_FRONT : integer range 0 to 6 := 6;
+     H_SYNC : integer range 0 to 34 := 34;
+     H_BACK : integer range 0 to 35 := 35;
+  -- V
+     V_VALID : integer range 0 to 768 := 768;
+     V_FRONT : integer range 0 to 3 := 3;
+     V_SYNC : integer range 0 to 6 := 6;
+     V_BACK : integer range 0 to 29 := 29;
+  -- Counter Width
+     H_CNT_WIDTH  : integer range 0 to 9 := 9;
+     V_CNT_WIDTH  : integer range 0 to 10 := 10;
+     H_OUT_WIDTH  : integer range 0 to 8 := 8;
+     V_OUT_WIDTH  : integer range 0 to 10 := 10
+   );
+
   port(
     clk_in, reset_in  : in std_logic;
     h_blank_out, v_blank_out, h_sync_out, v_sync_out  : out std_logic;
     h_addr_out  : out std_logic_vector(H_OUT_WIDTH-1 downto 0);
     v_addr_out  : out std_logic_vector(V_OUT_WIDTH-1 downto 0)
-  )
+  );
 end VideoTimingGen;
 
 architecture RTL of VideoTimingGen is
@@ -46,8 +47,8 @@ begin
       if(reset_in = '0')then
         h_cnt_reg <= conv_std_logic_vector(0, 9);
         v_cnt_reg <= conv_std_logic_vector(0, 10);
-      end if
-    end if
+      end if;
+    end if;
   end process;
 end RTL;
 
