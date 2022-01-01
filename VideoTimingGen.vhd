75c41b7faf999749b9aae2de21df1bd2c30e9f32
ponzu840w <ponzu840w@Redmax.localdomain>
2022-01-02 04:09:05 +0900

    VHDL化完了

--- VideoTimingGen.vhd
+++ VideoTimingGen.vhd
@@ -1,3 +1,4 @@
+-- Copyright 2021, 2022 @ponzu840w GPLv3.0
 -- Generate 1024*768 (XGA) H,V Sync.
 -- But address out is 256*768, because real dotclock is 16MHz.
 -- includes
@@ -8,54 +9,96 @@ use ieee.std_logic_arith.all;
 
 -- entity declaration
 entity VideoTimingGen is
-  -- constants
-  generic(
-  -- clock length
-  -- H
-     H_VALID : integer range 0 to 256 := 256;
-     H_FRONT : integer range 0 to 6 := 6;
-     H_SYNC : integer range 0 to 34 := 34;
-     H_BACK : integer range 0 to 35 := 35;
-  -- V
-     V_VALID : integer range 0 to 768 := 768;
-     V_FRONT : integer range 0 to 3 := 3;
-     V_SYNC : integer range 0 to 6 := 6;
-     V_BACK : integer range 0 to 29 := 29;
-  -- Counter Width
-     H_CNT_WIDTH  : integer range 0 to 9 := 9;
-     V_CNT_WIDTH  : integer range 0 to 10 := 10;
-     H_OUT_WIDTH  : integer range 0 to 8 := 8;
-     V_OUT_WIDTH  : integer range 0 to 10 := 10
-   );
-
   port(
-    clk_in, reset_in  : in std_logic;
-    h_blank_out, v_blank_out, h_sync_out, v_sync_out  : out std_logic;
-    h_addr_out  : out std_logic_vector(H_OUT_WIDTH-1 downto 0);
-    v_addr_out  : out std_logic_vector(V_OUT_WIDTH-1 downto 0)
-  );
+  clk_in, reset_in  : in std_logic;
+  h_blank_out, v_blank_out, h_sync_out, v_sync_out  : out std_logic; -- negative logic
+  h_addr_out  : out integer range 0 to 512;
+  v_addr_out  : out integer range 0 to 1024
+);
 end VideoTimingGen;
 
 architecture RTL of VideoTimingGen is
-  signal h_cnt_reg  : std_logic_vector(H_CNT_WIDTH-1 downto 0); -- 0~331 9bit counter
-  signal v_cnt_reg  : std_logic_vector(V_CNT_WIDTH-1 downto 0); -- 0~806 10bit counter
+  -- constants
+  -- clock length
+  -- H
+  constant H_VALID : integer range 0 to 256 := 256;
+  constant H_FRONT : integer range 0 to 6 := 6;
+  constant H_SYNC : integer range 0 to 34 := 34;
+  constant H_BACK : integer range 0 to 35 := 35;
+  -- V
+  constant V_VALID : integer range 0 to 768 := 768;
+  constant V_FRONT : integer range 0 to 3 := 3;
+  constant V_SYNC : integer range 0 to 6 := 6;
+  constant V_BACK : integer range 0 to 29 := 29;
+
+  signal h_cnt_reg  : integer range 0 to (H_VALID+H_FRONT+H_SYNC+H_BACK-1); -- 0~331 9bit counter
+  signal v_cnt_reg  : integer range 0 to (V_VALID+V_FRONT+V_SYNC+V_BACK-1); -- 0~806 10bit counter
 begin
-  u1:process(clk)
+  u1:process(clk_in)
   begin
     -- clk positive edge
-    if(clk'event and clk = '1')then
+    if(clk_in'event and clk_in = '1')then
       -- reset
       if(reset_in = '0')then
-        h_cnt_reg <= conv_std_logic_vector(0, H_CNT_WIDTH);
-        v_cnt_reg <= conv_std_logic_vector(0, V_CNT_WIDTH);
+        h_cnt_reg <= 0;
+        v_cnt_reg <= 0;
+        h_blank_out <= '1';
+        v_blank_out <= '1';
+        h_sync_out <= '1';
+        v_sync_out <= '1';
       else
-        case h_cnt_reg is
-          when conv_std_logic_vector(H_VALID-1) =>
-            h_cnt_reg <= "000000000";
-          when others =>
-            -- !?
-            h_cnt_reg <= "000000000";
-        end case;
+        -- end of h back
+        if(h_cnt_reg = H_VALID+H_FRONT+H_SYNC+H_BACK-1) then
+          h_blank_out <= '1';
+          h_cnt_reg <= 0;
+          -- end of v back
+          if(v_cnt_reg = V_VALID+V_FRONT+V_SYNC+V_BACK-1) then
+            v_blank_out <= '1';
+            v_cnt_reg <= 0;
+          else
+            -- v count (in end of h back)
+            v_cnt_reg <= v_cnt_reg+1;
+            v_addr_out <= v_cnt_reg+1;
+            case v_cnt_reg is
+              -- end of v valid
+              when V_VALID-1 =>
+                v_blank_out <= '0';
+
+              -- end of v front
+              when V_VALID+V_FRONT-1 =>
+                v_sync_out <= '0';
+
+              -- end of v sync
+              when V_VALID+V_FRONT+V_SYNC-1 =>
+                v_sync_out <= '1';
+
+              when others =>
+            -- ??
+            end case; -- end case about v
+          end if; -- end if(end of v)
+                  -- h valid
+        else -- not end of h back
+             -- count
+          h_cnt_reg <= h_cnt_reg+1;
+          h_addr_out <= h_cnt_reg+1;
+          -- about h count
+          case h_cnt_reg is
+            -- end of h valid
+            when H_VALID-1 =>
+              h_blank_out <= '0';
+
+            -- end of h front
+            when H_VALID+H_FRONT-1 =>
+              h_sync_out <= '0';
+
+            -- end of h sync
+            when H_VALID+H_FRONT+H_SYNC-1 =>
+              h_sync_out <= '1';
+
+            when others =>
+          -- ??
+          end case; --- end case about h
+        end if; -- end if(end of h)
       end if; -- end reset
     end if; -- end clk positive edge
   end process;
