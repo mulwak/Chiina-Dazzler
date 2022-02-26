2767fa0310aa4344e174093fccc4167cc907b8ef
mulwak <ponzu840w@Redmax.localdomain>
2022-02-27 02:37:50 +0900

    cpl信号を>>にすることで239 まだ収まらない

--- VideoTimingGen.vhd
+++ VideoTimingGen.vhd
@@ -11,6 +11,7 @@ entity VideoTimingGen is
   port(
   clk_in, reset_in  : in std_logic;
   h_blank_out, v_blank_out, h_sync_out, v_sync_out  : out std_logic; -- negative logic
+  h_earlyblank_out, v_earlyblank_out  : out std_logic;
   h_addr_out  : out integer range 0 to 511;
   v_addr_out  : out integer range 0 to 1023;
   cpload_out :  out std_logic
@@ -31,13 +32,22 @@ architecture RTL of VideoTimingGen is
   constant V_SYNC : integer := 6;
   constant V_BACK : integer := 28;
 
-  constant CPLOAD_END : integer := H_VALID+24+24-4;
+  constant CPLOAD_END : integer := H_VALID+24+24;
 
   signal h_cnt_reg  : integer range 0 to (H_VALID+H_FRONT+H_SYNC+H_BACK-1); -- 0~331 9bit counter
   signal v_cnt_reg  : integer range 0 to (V_VALID+V_FRONT+V_SYNC+V_BACK-1); -- 0~806 10bit counter
+  signal h_blank_delayreg0, v_blank_delayreg0 : std_logic;
+  signal h_blank_delayreg1, v_blank_delayreg1 : std_logic;
+  signal h_eblank_reg, v_eblank_reg : std_logic;
 begin
   h_addr_out <= h_cnt_reg;
   v_addr_out <= v_cnt_reg;
+  h_blank_out <= h_blank_delayreg0;
+  v_blank_out <= v_blank_delayreg0;
+  h_earlyblank_out <= h_eblank_reg;
+  v_earlyblank_out <= v_eblank_reg;
+    cpload_out <= '1' when H_VALID-1 < h_cnt_reg and  h_cnt_reg < CPLOAD_END else
+                  '0';
   u1:process(clk_in)
   begin
     -- clk positive edge
@@ -46,35 +56,26 @@ begin
       if(reset_in = '0')then
         h_cnt_reg <= 0;
         v_cnt_reg <= 0;
+        --h_eblank_reg <= '1';
+        --v_eblank_reg <= '1';
         --h_sync_out <= '1';
         --v_sync_out <= '1';
-        --v_blank_out <= '1';
       else
-        -- count
-        case h_cnt_reg is
-          when H_VALID+H_FRONT+H_SYNC+H_BACK-1 =>
-            h_cnt_reg <= 0;
-          when others =>
-            h_cnt_reg <= h_cnt_reg+1;
-        end case;
-
-        case h_cnt_reg is
-          when 1 =>
-          --when H_VALID+H_FRONT+H_SYNC+H_BACK-1 =>
-
+        -- end of h back
+        if(h_cnt_reg = H_VALID+H_FRONT+H_SYNC+H_BACK-1) then
+          h_eblank_reg <= '1';
+          h_cnt_reg <= 0;
+          -- end of v back
+          if(v_cnt_reg = V_VALID+V_FRONT+V_SYNC+V_BACK-1) then
+            v_eblank_reg <= '1';
+            v_cnt_reg <= 0;
+          else
+            -- v count (in end of h back)
+            v_cnt_reg <= v_cnt_reg+1;
             case v_cnt_reg is
-              when V_VALID+V_FRONT+V_SYNC+V_BACK-1 =>
-                v_cnt_reg <= 0;
-              when others =>
-                v_cnt_reg <= v_cnt_reg+1;
-            end case;
-
-            case v_cnt_reg is
-              when V_VALID+V_FRONT+V_SYNC+V_BACK-1 =>
-                v_blank_out <= '1';
               -- end of v valid
               when V_VALID-1 =>
-                v_blank_out <= '0';
+                v_eblank_reg <= '0';
 
               -- end of v front
               when V_VALID+V_FRONT-1 =>
@@ -87,30 +88,34 @@ begin
               when others =>
             -- ??
             end case; -- end case about v
-
-          -- end of h valid
-            h_blank_out <= '1';
-
-          when 257 =>
-            h_blank_out <= '0';
-
-          when H_VALID-1 =>
-            cpload_out <= '1';
+          end if; -- end if(end of v)
+                  -- h valid
+        else -- not end of h back
+             -- count
+          h_cnt_reg <= h_cnt_reg+1;
+          -- about h count
+          case h_cnt_reg is
+            -- end of h valid
+            when H_VALID-1 =>
+              h_eblank_reg <= '0';
 
             -- end of h front
-          when H_VALID+H_FRONT =>
-            h_sync_out <= '0';
+            when H_VALID+H_FRONT =>
+              h_sync_out <= '0';
 
             -- end of h sync
-          when H_VALID+H_FRONT+H_SYNC =>
-            h_sync_out <= '1';
+            when H_VALID+H_FRONT+H_SYNC =>
+              h_sync_out <= '1';
 
-          when CPLOAD_END =>
-            cpload_out <= '0';
-
-          when others =>
-        end case;
+            when others =>
+          -- ??
+          end case; --- end case about h
+        end if; -- end if(end of h)
       end if; -- end reset
+    h_blank_delayreg1 <= h_eblank_reg;
+    h_blank_delayreg0 <= h_blank_delayreg1;
+    v_blank_delayreg1 <= v_eblank_reg;
+    v_blank_delayreg0 <= v_blank_delayreg1;
     end if; -- end clk positive edge
   end process;
 end RTL;
